83cedaa466d31433a90b0254b7b2253d2797b158
bborn <bruno.bornsztein@gmail.com>
2010-11-18 07:58:08 -0600

    fix http timeouts

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -13,7 +13,7 @@ module TinyMCE
   end
 end
 
-TinyMCE::OptionValidator.valid_options = ['atd_rpc_id', 'atd_rpc_url', 'atd_css_url', 'atd_button_url', 'atd_autocheck']  
+TinyMCE::OptionValidator.valid_options = ['atd_ignore_strings', 'atd_rpc_id', 'atd_rpc_url', 'atd_css_url', 'atd_button_url', 'atd_autocheck']  
 
 AppConfig.default_mce_options.merge!({
   :theme_advanced_buttons1_add => %w{AtD},
@@ -21,15 +21,21 @@ AppConfig.default_mce_options.merge!({
   :atd_rpc_id => AppConfig.atd_spellcheck_key,
   :atd_rpc_url => '/base/atd_spellcheck?url=',
   :atd_css_url => '/plugin_assets/community_engine_spellcheck/javascripts/atd-tinymce/css/content.css',
-  :atd_button_url => "/plugin_assets/community_engine_spellcheck/javascripts/atd-tinymce/atdbuttontr.gif"  
+  :atd_button_url => "/plugin_assets/community_engine_spellcheck/javascripts/atd-tinymce/atdbuttontr.gif"
 })
 
 class BaseController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:atd_spellcheck]
 
   def atd_spellcheck
-    response = Net::HTTP.post_form(URI.parse("http://service.afterthedeadline.com#{params[:url]}"), {'data'=>params[:data], 'key'=> AppConfig.atd_spellcheck_key})
-    render :xml => response.body
+    begin
+      response = Net::HTTP.post_form(URI.parse("http://service.afterthedeadline.com#{params[:url]}"), {'data'=>params[:data], 'key'=> AppConfig.atd_spellcheck_key})
+      render :xml => response.body
+    rescue Timeout::Error
+      #timeouts happen because ATD is down or slow, so we don't want to stop the user from saving a post as a result
+      render :xml => '<results></results>'
+    end
+
   end
 
 end
